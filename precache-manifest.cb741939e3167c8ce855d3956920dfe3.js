44e316cae72665f144d0951698480e1c86d5d792
Liger <jtadub@gmail.com>
2020-09-23 15:14:07 +1200

    Updates

---
+++ precache-manifest.cb741939e3167c8ce855d3956920dfe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528d3e06e5a604ea1104fcaf9e824879",
+    "url": "/library-react/index.html"
+  },
+  {
+    "revision": "8d67bc93f3e1d7d4cb53",
+    "url": "/library-react/static/css/main.34291dee.chunk.css"
+  },
+  {
+    "revision": "22e6e2c4a3c3441a7f56",
+    "url": "/library-react/static/js/2.735dc970.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/library-react/static/js/2.735dc970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d67bc93f3e1d7d4cb53",
+    "url": "/library-react/static/js/main.5381007a.chunk.js"
+  },
+  {
+    "revision": "5eab4539a2b11719e949",
+    "url": "/library-react/static/js/runtime-main.bc126f7f.js"
+  }
+]);
